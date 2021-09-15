0e51a02f3e3c0b146c34b27b404869fbaee20d26
Sebastien Boeuf <sebastien.boeuf@intel.com>
2021-09-15 15:39:54 +0200

    Serialize various structures for migration
    Serialization is mostly needed by the VMM consuming this crate in order
    to support snapshot/restore and live migration.
    
    Unit tests are added for both architectures.
    
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>
    Signed-off-by: Henry Wang <Henry.Wang@arm.com>
    Signed-off-by: Michael Zhao <michael.zhao@arm.com>
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Bo Chen <chen.bo@intel.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,12 @@
 #[cfg(feature = "fam-wrappers")]
 extern crate vmm_sys_util;
 
+#[cfg(feature = "with-serde")]
+extern crate serde;
+
+#[cfg(feature = "with-serde")]
+extern crate serde_derive;
+
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod x86;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
@@ -20,3 +26,25 @@ pub use self::x86::*;
 mod arm64;
 #[cfg(any(target_arch = "aarch", target_arch = "aarch64"))]
 pub use self::arm64::*;
+
+#[cfg(all(test, feature = "with-serde"))]
+mod tests {
+    extern crate serde_json;
+
+    use super::Version;
+
+    #[test]
+    fn test_version_ser_deser() {
+        let ver = Version {
+            arch: "x86_64",
+            kernel_ver: "v5.13.0",
+            crate_ver: "v0.5.0",
+        };
+        let ver_str = "{\"arch\":\"x86_64\",\"kernel_ver\":\"v5.13.0\",\"crate_ver\":\"v0.5.0\"}";
+        let ver_ser = serde_json::to_string(&ver).unwrap();
+        assert_eq!(ver_ser, ver_str.to_string());
+
+        let ver_deser = serde_json::from_str::<Version>(ver_str).unwrap();
+        assert_eq!(ver, ver_deser);
+    }
+}
